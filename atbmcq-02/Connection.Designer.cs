cc527fc99ca07774e98a6a1cd2444ea85dfdb00e
phuc-dep-trai <animebosses02@gmail.com>
2025-04-27 10:40:49 +0700

    Update connection handling: Improve OracleConnection class and Connection form

--- atbmcq-02/Connection.Designer.cs
+++ atbmcq-02/Connection.Designer.cs
@@ -43,7 +43,6 @@ namespace atbmcq_02
             cboRole = new ComboBox();
             lblPassword = new Label();
             txtPassword = new TextBox();
-            chkSavePassword = new CheckBox();
             lblConnType = new Label();
             cboConnType = new ComboBox();
             lblHostname = new Label();
@@ -56,30 +55,10 @@ namespace atbmcq_02
             txtServiceName = new TextBox();
             btnConnect = new Button();
             btnTest = new Button();
-            btnClear = new Button();
             btnCancel = new Button();
-            btnSave = new Button();
-            label1 = new Label();
-            lstSavedAccounts = new ListView();
-            colUsername = new ColumnHeader();
-            colHostname = new ColumnHeader();
-            colRole = new ColumnHeader();
-            accountContextMenu = new ContextMenuStrip(components);
-            editMenuItem = new ToolStripMenuItem();
-            deleteMenuItem = new ToolStripMenuItem();
-            splitContainer = new SplitContainer();
-            listButtonPanel = new Panel();
-            btnEdit = new Button();
-            btnDelete = new Button();
             bottomPanel = new Panel();
             tabControl.SuspendLayout();
             tabDetails.SuspendLayout();
-            accountContextMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
-            splitContainer.Panel1.SuspendLayout();
-            splitContainer.Panel2.SuspendLayout();
-            splitContainer.SuspendLayout();
-            listButtonPanel.SuspendLayout();
             bottomPanel.SuspendLayout();
             SuspendLayout();
             // 
@@ -104,7 +83,6 @@ namespace atbmcq_02
             tabDetails.Controls.Add(cboRole);
             tabDetails.Controls.Add(lblPassword);
             tabDetails.Controls.Add(txtPassword);
-            tabDetails.Controls.Add(chkSavePassword);
             tabDetails.Controls.Add(lblConnType);
             tabDetails.Controls.Add(cboConnType);
             tabDetails.Controls.Add(lblHostname);
@@ -175,7 +153,7 @@ namespace atbmcq_02
             // 
             cboRole.DropDownStyle = ComboBoxStyle.DropDownList;
             cboRole.FormattingEnabled = true;
-            cboRole.Items.AddRange(new object[] { "default", "SYSDBA", "SYSOPER", "SYSBACKUP", "SYSDG", "SYSKM", "SYSRAC" });
+            cboRole.Items.AddRange(new object[] { "default" });
             cboRole.Location = new Point(784, 81);
             cboRole.Margin = new Padding(4, 3, 4, 3);
             cboRole.Name = "cboRole";
@@ -200,16 +178,6 @@ namespace atbmcq_02
             txtPassword.PasswordChar = '*';
             txtPassword.Size = new Size(390, 37);
             txtPassword.TabIndex = 7;
-            // 
-            // chkSavePassword
-            // 
-            chkSavePassword.AutoSize = true;
-            chkSavePassword.Location = new Point(700, 139);
-            chkSavePassword.Margin = new Padding(4, 3, 4, 3);
-            chkSavePassword.Name = "chkSavePassword";
-            chkSavePassword.Size = new Size(196, 33);
-            chkSavePassword.TabIndex = 8;
-            chkSavePassword.Text = "Save Password";
             // 
             // lblConnType
             // 
@@ -316,166 +284,33 @@ namespace atbmcq_02
             // 
             // btnConnect
             // 
-            btnConnect.Location = new Point(1231, 17);
+            btnConnect.Location = new Point(621, 17);
             btnConnect.Margin = new Padding(4, 3, 4, 3);
             btnConnect.Name = "btnConnect";
             btnConnect.Size = new Size(140, 41);
-            btnConnect.TabIndex = 3;
+            btnConnect.TabIndex = 2;
             btnConnect.Text = "Connect";
             // 
             // btnTest
             // 
-            btnTest.Location = new Point(977, 17);
+            btnTest.Location = new Point(341, 17);
             btnTest.Margin = new Padding(4, 3, 4, 3);
             btnTest.Name = "btnTest";
             btnTest.Size = new Size(140, 41);
-            btnTest.TabIndex = 2;
+            btnTest.TabIndex = 1;
             btnTest.Text = "Test";
-            // 
-            // btnClear
-            // 
-            btnClear.Location = new Point(722, 17);
-            btnClear.Margin = new Padding(4, 3, 4, 3);
-            btnClear.Name = "btnClear";
-            btnClear.Size = new Size(140, 41);
-            btnClear.TabIndex = 1;
-            btnClear.Text = "Clear";
             // 
             // btnCancel
             // 
-            btnCancel.Location = new Point(1486, 17);
+            btnCancel.Location = new Point(901, 17);
             btnCancel.Margin = new Padding(4, 3, 4, 3);
             btnCancel.Name = "btnCancel";
             btnCancel.Size = new Size(140, 41);
             btnCancel.TabIndex = 4;
             btnCancel.Text = "Cancel";
             // 
-            // btnSave
-            // 
-            btnSave.Location = new Point(468, 17);
-            btnSave.Margin = new Padding(4, 3, 4, 3);
-            btnSave.Name = "btnSave";
-            btnSave.Size = new Size(140, 41);
-            btnSave.TabIndex = 0;
-            btnSave.Text = "Save";
-            // 
-            // label1
-            // 
-            label1.Location = new Point(0, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(100, 23);
-            label1.TabIndex = 0;
-            // 
-            // lstSavedAccounts
-            // 
-            lstSavedAccounts.Columns.AddRange(new ColumnHeader[] { colUsername, colHostname, colRole });
-            lstSavedAccounts.ContextMenuStrip = accountContextMenu;
-            lstSavedAccounts.Dock = DockStyle.Fill;
-            lstSavedAccounts.FullRowSelect = true;
-            lstSavedAccounts.GridLines = true;
-            lstSavedAccounts.Location = new Point(0, 0);
-            lstSavedAccounts.Margin = new Padding(4, 3, 4, 3);
-            lstSavedAccounts.Name = "lstSavedAccounts";
-            lstSavedAccounts.Size = new Size(420, 476);
-            lstSavedAccounts.TabIndex = 0;
-            lstSavedAccounts.UseCompatibleStateImageBehavior = false;
-            lstSavedAccounts.View = View.Details;
-            lstSavedAccounts.SelectedIndexChanged += lstSavedAccounts_SelectedIndexChanged;
-            // 
-            // colUsername
-            // 
-            colUsername.Text = "Username";
-            colUsername.Width = 140;
-            // 
-            // colHostname
-            // 
-            colHostname.Text = "Hostname";
-            colHostname.Width = 140;
-            // 
-            // colRole
-            // 
-            colRole.Text = "Role";
-            colRole.Width = 140;
-            // 
-            // accountContextMenu
-            // 
-            accountContextMenu.ImageScalingSize = new Size(24, 24);
-            accountContextMenu.Items.AddRange(new ToolStripItem[] { editMenuItem, deleteMenuItem });
-            accountContextMenu.Name = "accountContextMenu";
-            accountContextMenu.Size = new Size(135, 68);
-            // 
-            // editMenuItem
-            // 
-            editMenuItem.Name = "editMenuItem";
-            editMenuItem.Size = new Size(134, 32);
-            editMenuItem.Text = "Edit";
-            editMenuItem.Click += EditAccount_Click;
-            // 
-            // deleteMenuItem
-            // 
-            deleteMenuItem.Name = "deleteMenuItem";
-            deleteMenuItem.Size = new Size(134, 32);
-            deleteMenuItem.Text = "Delete";
-            deleteMenuItem.Click += DeleteAccount_Click;
-            // 
-            // splitContainer
-            // 
-            splitContainer.Location = new Point(38, 23);
-            splitContainer.Margin = new Padding(4, 3, 4, 3);
-            splitContainer.Name = "splitContainer";
-            // 
-            // splitContainer.Panel1
-            // 
-            splitContainer.Panel1.Controls.Add(lstSavedAccounts);
-            splitContainer.Panel1.Controls.Add(listButtonPanel);
-            // 
-            // splitContainer.Panel2
-            // 
-            splitContainer.Panel2.Controls.Add(tabControl);
-            splitContainer.Size = new Size(1592, 522);
-            splitContainer.SplitterDistance = 420;
-            splitContainer.SplitterWidth = 6;
-            splitContainer.TabIndex = 0;
-            // 
-            // listButtonPanel
-            // 
-            listButtonPanel.Controls.Add(btnEdit);
-            listButtonPanel.Controls.Add(btnDelete);
-            listButtonPanel.Dock = DockStyle.Bottom;
-            listButtonPanel.Location = new Point(0, 476);
-            listButtonPanel.Margin = new Padding(4, 3, 4, 3);
-            listButtonPanel.Name = "listButtonPanel";
-            listButtonPanel.Size = new Size(420, 46);
-            listButtonPanel.TabIndex = 1;
-            // 
-            // btnEdit
-            // 
-            btnEdit.Dock = DockStyle.Left;
-            btnEdit.Enabled = false;
-            btnEdit.Location = new Point(0, 0);
-            btnEdit.Margin = new Padding(4, 3, 4, 3);
-            btnEdit.Name = "btnEdit";
-            btnEdit.Size = new Size(210, 46);
-            btnEdit.TabIndex = 0;
-            btnEdit.Text = "Edit";
-            btnEdit.Click += EditAccount_Click;
-            // 
-            // btnDelete
-            // 
-            btnDelete.Dock = DockStyle.Right;
-            btnDelete.Enabled = false;
-            btnDelete.Location = new Point(218, 0);
-            btnDelete.Margin = new Padding(4, 3, 4, 3);
-            btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(202, 46);
-            btnDelete.TabIndex = 1;
-            btnDelete.Text = "Delete";
-            btnDelete.Click += DeleteAccount_Click;
-            // 
             // bottomPanel
             // 
-            bottomPanel.Controls.Add(btnSave);
-            bottomPanel.Controls.Add(btnClear);
             bottomPanel.Controls.Add(btnTest);
             bottomPanel.Controls.Add(btnConnect);
             bottomPanel.Controls.Add(btnCancel);
@@ -491,7 +326,7 @@ namespace atbmcq_02
             AutoScaleDimensions = new SizeF(14F, 29F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1662, 626);
-            Controls.Add(splitContainer);
+            Controls.Add(tabControl);
             Controls.Add(bottomPanel);
             Font = new Font("Times New Roman", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Margin = new Padding(4, 3, 4, 3);
@@ -500,12 +335,6 @@ namespace atbmcq_02
             tabControl.ResumeLayout(false);
             tabDetails.ResumeLayout(false);
             tabDetails.PerformLayout();
-            accountContextMenu.ResumeLayout(false);
-            splitContainer.Panel1.ResumeLayout(false);
-            splitContainer.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
-            splitContainer.ResumeLayout(false);
-            listButtonPanel.ResumeLayout(false);
             bottomPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
@@ -528,7 +357,6 @@ namespace atbmcq_02
         private ComboBox cboRole;
         private Label lblPassword;
         private TextBox txtPassword;
-        private CheckBox chkSavePassword;
         private Label lblConnType;
         private ComboBox cboConnType;
         private Label lblHostname;
@@ -541,21 +369,7 @@ namespace atbmcq_02
         private TextBox txtServiceName;
         private Button btnConnect;
         private Button btnTest;
-        private Button btnClear;
         private Button btnCancel;
-        private Button btnSave;
-        private Label label1;
-        private SplitContainer splitContainer;
-        private ListView lstSavedAccounts;
-        private ColumnHeader colUsername;
-        private ColumnHeader colHostname;
-        private ColumnHeader colRole;
-        private Button btnEdit;
-        private Button btnDelete;
-        private ContextMenuStrip accountContextMenu;
-        private ToolStripMenuItem editMenuItem;
-        private ToolStripMenuItem deleteMenuItem;
-        private Panel listButtonPanel;
         private Panel bottomPanel;
     }
 }
