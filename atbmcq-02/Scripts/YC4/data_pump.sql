d68fe1cdf12cd12fd478a6dc72319e547a1c3754
Nguyen Minh Tam <tamteo6@gmail.com>
2025-06-01 10:52:08 +0700

    Them test datapump

--- atbmcq-02/Scripts/YC4/data_pump.sql
+++ atbmcq-02/Scripts/YC4/data_pump.sql
@@ -5,29 +5,44 @@
 -- Bước 1: Tạo thư mục để chứa các tệp sao lưu
 -- mkdir -p /opt/oracle/backups_test/dpump
 -- Bước 2: Cấp quyền cho Oracle user để có thể ghi vào thư mục này
--- Kết nối vào sqlplus với quyền SYSDBA: sqlplus / C##ADMIN/123456@localhost:1521/<service_name>
+-- Kết nối vào sqlplus với quyền SYSDBA: sqlplus / as sysdba
 -- Chạy sql: 
-/*
-CREATE OR REPLACE DIRECTORY dpump_dir AS '/opt/oracle/backups_test/dpump';
-GRANT READ, WRITE ON DIRECTORY dpump_dir TO C##ADMIN;
-exit;
-*/
+-- CREATE OR REPLACE DIRECTORY dpump_dir AS '/opt/oracle/backups_test/dpump';
+-- GRANT READ, WRITE ON DIRECTORY dpump_dir TO C##ADMIN;
+
+
+-- Chuẩn bị dữ liệu đã test
+-- CREATE USER C##dptest IDENTIFIED BY 123;
+-- GRANT CONNECT, RESOURCE TO C##dptest;
+
+-- Kết nối vào sqlplus với user C##dptest: sqlplus C##dptest/123@localhost:1521/<service_name>
+-- Sau đó tạo bảng 
+-- CREATE TABLE employees (
+--    id NUMBER PRIMARY KEY,
+--    name VARCHAR2(100),
+--    salary NUMBER
+--);
 
 -- ============================================================
 --                   Sao lưu bằng Data Pump
 -- ============================================================
 
 -- Bước 1: Mở cmd và chạy Data Pump Export
--- expdp C##ADMIN/123456@localhost:1521/<service_name> DIRECTORY=dpump_dir DUMPFILE=schema_backup.dmp LOGFILE=export.log SCHEMAS=C##ADMIN
+-- expdp C##ADMIN/123456@localhost:1521/<service_name> DIRECTORY=dpump_dir DUMPFILE=schema_backups.dmp LOGFILE=export_schemas.log SCHEMAS=C##dptest
 -- Bước 2: Kiểm tra tệp sao lưu đã được tạo: ls /opt/oracle/backups_test/dpump/schema_backup.dmp 
 
+-- Kết nối vào sqlplus với quyền SYSDBA: sqlplus C##ADMIN/123456@localhost:1521/<service_name>\
 -- Tạo tình huống mất dữ liệu
--- Tâm chưa nghĩ ra tình huống mất dữ liệu để test Data Pump
+DROP USER C##dptest CASCADE;
+
+-- Thử truy vấn bảng trong C##dptest sẽ báo lỗi
+SELECT * FROM C##dptest.employees;
 
 -- ============================================================
 --                   Phục hồi bằng Data Pump
 -- ============================================================
 
 -- Bước 1: Mở cmd và chạy Data Pump Import
--- impdp C##ADMIN/123456@localhost:1521/<service_name> DIRECTORY=dpump_dir DUMPFILE=schema_backup.dmp LOGFILE=import.log SCHEMAS=C##ADMIN
+-- impdp C##ADMIN/123456@localhost:1521/<service_name> DIRECTORY=dpump_dir DUMPFILE=schema_backups.dmp LOGFILE=export_schemas.log SCHEMAS=C##dptest
 -- Bước 2: Kiểm tra dữ liệu sau phục hồi
+SELECT * FROM C##dptest.employees;
