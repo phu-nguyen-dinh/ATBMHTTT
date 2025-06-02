013ff8b21138f68c06719e56b92c996d6fa0687c
Nguyen Minh Tam <tamteo6@gmail.com>
2025-06-02 12:09:44 +0700

    Sua loi

--- atbmcq-02/Scripts/YC4/rman.sql
+++ atbmcq-02/Scripts/YC4/rman.sql
@@ -9,16 +9,16 @@ ARCHIVE LOG LIST;
 */
 
 -- Tạo thư mục để chứa các tệp sao lưu
--- mkdir -p /opt/oracle/backup_rman/test
+-- mkdir -p /opt/oracle/rman_bk/tests
 
 -- ===========================================================
 --                   Sao lưu bằng RMAN
 -- ===========================================================
 
--- Bước 1: Mở cmd và chạy RMAN: rman target C##ADMIN/123456@localhost:1521/<service_name>
+-- Bước 1: Mở cmd và chạy RMAN: rman target /
 -- Bước 2: Thực hiện sao lưu toàn bộ database:
--- RMAN> BACKUP DATABASE FORMAT '/opt/oracle/backup_rman/testfulldb_test.bkp';
--- RAMN> BACKUP ARCHIVELOG ALL FORMAT '/opt/oracle/backup_rman/test/archlog_test.bkp';
+-- RMAN> BACKUP DATABASE FORMAT '/opt/oracle/rman_bk/tests/db_%U.bkp';
+-- RAMN> BACKUP ARCHIVELOG ALL FORMAT '/opt/oracle/rman_bk/tests/arch_%U.bkp';
 -- Bước 3: Kiểm tra danh sách các bản sao lưu:
 -- RMAN> LIST BACKUP OF DATABASE;
  
@@ -32,7 +32,7 @@ SELECT * FROM C##ADMIN.DONVI;
  -- ============================================================
 --                   Phục hồi bằng RMAN
 -- ============================================================
--- Bước 1: Mở cmd và chạy RMAN: rman target C##ADMIN/123456@localhost:1521/<service_name>
+-- Bước 1: Mở cmd và chạy RMAN: rman target /
 -- Bước 2: Chạy lệnh phục hồi:
 -- RMAN> RUN {
 --    SET UNTIL TIME "TO_DATE('2025-05-31 10:00:00','YYYY-MM-DD HH24:MI:SS')"; -- Dòng này để khôi phục đến thời điểm cụ thể(có thể dựa vào audit) và có thể bỏ qua nếu muốn phục hồi đến bản sao lưu mới nhất
